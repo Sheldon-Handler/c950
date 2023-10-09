87fe7111ac6e39ef41389b8b62f731fab665085e
Sheldon Handler <57599804+Sheldon-Handler@users.noreply.github.com>
2023-10-09 13:38:03 -0400

    Need to work on fixing

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # Student ID: 007830903
 # Name: Sheldon Handler
+import main
 import package
 import hash_table
 import status
 
-newPackage = package.Package.__init__(self=package.Package, address="9732", city="brook", state="nt", postal="11210", note="s", status=status.Status.NOT_AVAILABLE, deadline="12:30", weight=42)
+newPackage = package.Package.__init__(address="9732", city="brook", state="nt", postal="11210", note="s", status=status.Status.NOT_AVAILABLE, deadline="12:30", weight=42)
 
-print(newPackage)
+print(newPackage.__str__())
--- package.py
+++ package.py
@@ -1,8 +1,7 @@
 """
 This file stores the template for object package
 """
-import this
-
+import package
 from status import Status
 
 
@@ -10,17 +9,9 @@ class Package:
     """
 This class stores information about a package.
     """
-    address: str
-    city: str
-    state: str
-    postal: str
-    country: str
-    weight: int
-    deadline: str
-    note: str
 
-    def __init__(self, address: str, city: str, state: str, postal: str, weight: int,
-                 deadline: str, note: str, status: Status):
+    def __init__(self=package, address: str = "", city: str = "", state: str = "", postal: str = "", weight: int = None,
+                 deadline: str = "", note: str = "", status: Status = Status.NOT_AVAILABLE):
         """
 
         :param address: address for delivering package
@@ -32,11 +23,14 @@ This class stores information about a package.
         :param note: special note for package
         """
 
-        super.address = address
-        city = city
-        state = state
-        postal = postal
-        weight = weight
-        deadline = deadline
-        Package.note = note
-        Package.status = status
+        self.address = address
+        self.city = city
+        self.state = state
+        self.postal = postal
+        self.weight = weight
+        self.deadline = deadline
+        self.note = note
+        self.status = status
+
+    def __str__(self=package):
+        return f"{self.address}, {self.city}, {self.state}, {self.postal}, {self.weight}, {self.deadline}, {self.note}, {self.status}"
