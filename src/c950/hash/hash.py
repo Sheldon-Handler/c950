b2549155bfcf635d5bf0db20e7d3084c6947169f
Sheldon_Handler <57599804+Sheldon-Handler@users.noreply.github.com>
2023-10-19 18:40:26 -0400

    Working on hash

--- src/c950/hash/hash.py
+++ src/c950/hash/hash.py
@@ -3,9 +3,14 @@
 """
 
 
+# Hash class to store objects
 class Hash:
 
+    # Constructor
     def __init__(self):
         self.size = 10
         self.table = [None] * self.size
+
+    def hash_function(self, key):
+        return key % self.size
 
