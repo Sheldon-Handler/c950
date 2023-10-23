1cab932fe49511690d97f382a1fdf6735b7bade8
Sheldon Handler <57599804+Sheldon-Handler@users.noreply.github.com>
2023-10-23 19:07:46 -0400

    Working on some things

--- src/c950/package.py
+++ src/c950/package.py
@@ -7,75 +7,62 @@
 #  The above copyright notice and this permission notice (including the next paragraph) shall be included in all copies or substantial portions of the Software.
 #
 #  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-import sqlite3
+
+import enum
+import dataclasses
+import time
 
 
+class DeliveryStatus(enum.Enum):
+    """This enum class represents the delivery status of a package.
+
+    Attributes:
+        NOT_AVAILABLE (int): The package is not available.
+        AT_HUB (int): The package is at the hub.
+        EN_ROUTE (int): The package is en route.
+        DELIVERED (int): The package has been delivered.
+
+    See Also:
+        https://docs.python.org/3/library/enum.html
+    """
+
+    NOT_AVAILABLE = 0
+    AT_HUB = 1
+    EN_ROUTE = 2
+    DELIVERED = 3
+
+
+@dataclasses.dataclass
 class Package:
     """This class represents a package object with attributes representing the
     details of the package.
 
+    Attributes:
+        package_id (int): The package id.
+        address (str): The package address.
+        city (str): The package city.
+        state (str): The package state.
+        zip (str): The package zip code.
+        weight_kilo (int): The package weight in kilos.
+        delivery_deadline (time): The package delivery deadline.
+        special_notes (str): The package special notes.
+        delivery_status (str): The package delivery status.
+        delivery_truck (int): The package delivery truck.
+        delivery_time (time): The package delivery time.
+
     See Also:
         https://docs.python.org/3/library/dataclasses.html
+        https://docs.python.org/3/library/time.html
     """
 
-    # Constructor
-    def __init__(
-        self,
-        package_id: int,
-        address: str,
-        city: str,
-        state: str,
-        zip: str,
-        weight_kilo: int,
-        delivery_deadline: sqlite3.Time,
-        special_notes: str,
-        delivery_status: str,
-        delivery_truck: int,
-        delivery_time: sqlite3.Time,
-    ):
-        """Initialize the Package instance.
-
-        Args:
-            self (Package): The Package instance to initialize.
-            package_id (int): The package ID.
-            address (str): The package address.
-            city (str): The package city.
-            state (str): The package state.
-            zip (str): The package zip code.
-            weight_kilo (int): The package KILO weight.
-            delivery_deadline (sqlite3.Time): The package delivery deadline.
-            special_notes (str): The package special notes.
-            delivery_status (str): The package delivery status.
-            delivery_truck (int): The package delivery truck.
-            delivery_time (sqlite3.Time): The package delivery time.
-
-        Returns:
-            Package: A Package instance.
-        """
-
-        self.package_id = package_id
-        self.address = address
-        self.city = city
-        self.state = state
-        self.zip = zip
-        self.weight_kilo = weight_kilo
-        self.delivery_deadline = delivery_deadline
-        self.special_notes = special_notes
-        self.delivery_status = delivery_status
-        self.delivery_truck = delivery_truck
-        self.delivery_time = delivery_time
-
-    def to_list(self):
-        return [
-            self.package_id,
-            self.address,
-            self.city,
-            self.state,
-            self.zip,
-            self.weight_kilo,
-            self.delivery_deadline,
-            self.special_notes,
-            self.delivery_status,
-            self.delivery_truck,
-            self.delivery_time,
-        ]
+    package_id: int
+    address: str
+    city: str
+    state: str
+    zip: str
+    weight_kilo: int
+    delivery_deadline: time
+    special_notes: str
+    delivery_status: DeliveryStatus
+    delivery_truck: int
+    delivery_time: time
