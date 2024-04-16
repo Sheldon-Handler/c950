025f332edbbfd49517db385957ca5c43a3aec1f1
Sheldon_Handler <57599804+Sheldon-Handler@users.noreply.github.com>
2024-04-16 13:33:39 -0400

    Reorganized hash_table.py

--- data_structures_and_algorithms_ii/hash_table.py
+++ data_structures_and_algorithms_ii/hash_table.py
@@ -55,7 +55,39 @@ class HashTable:
                 worst case = O(1)
                 average case = O(1)
         """
-        return hash(key) % len(self.table)  # Hash the key and return the hash value
+        # Hash the key and return the hash value
+        return hash(key) % len(self.table)
+
+    def get(self, key) -> any:
+        """
+        Gets the value for the given key in the hash table.
+
+        Args:
+            key: The key to get the value for.
+
+        Returns:
+            any: The value for the given key.
+
+        Notes:
+            time complexity:
+                best case = O(1)
+                worst case = O(n)
+                average case = O(n)
+            space complexity:
+                best case = O(1)
+                worst case = O(1)
+                average case = O(1)
+        """
+        # Hash the key to get the hash value
+        hash_value = self._hash(key)
+
+        # Search for the key in the hash table and return the value if found
+        for i in self.table[hash_value]:  # O(n) - for loop
+            if self.table[hash_value][i][0] == key:
+                return self.table[hash_value][i][1]
+
+        # If the key is not found, return None
+        return None
 
     def set(self, key, value) -> None:
         """
@@ -88,34 +120,3 @@ class HashTable:
 
         # If the key does not exist, append the key-value pair to the hash table
         self.table[hash_value].append((key, value))
-
-    def get(self, key) -> any:
-        """
-        Gets the value for the given key in the hash table.
-
-        Args:
-            key: The key to get the value for.
-
-        Returns:
-            any: The value for the given key.
-
-        Notes:
-            time complexity:
-                best case = O(1)
-                worst case = O(n)
-                average case = O(n)
-            space complexity:
-                best case = O(1)
-                worst case = O(1)
-                average case = O(1)
-        """
-        # Hash the key to get the hash value
-        hash_value = self._hash(key)
-
-        # Search for the key in the hash table and return the value if found
-        for i in self.table[hash_value]:  # O(n) - for loop
-            if self.table[hash_value][i][0] == key:
-                return self.table[hash_value][i][1]
-
-        # If the key is not found, return None
-        return None
