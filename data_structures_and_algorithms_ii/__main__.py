46d25844b80920f20f90a81d69c2a9101e7e4df6
Sheldon_Handler <57599804+Sheldon-Handler@users.noreply.github.com>
2024-04-19 11:55:56 -0400

    Removed redundent unused variable

--- data_structures_and_algorithms_ii/__main__.py
+++ data_structures_and_algorithms_ii/__main__.py
@@ -6,8 +6,6 @@ import data_structures_and_algorithms_ii
 newShell = data_structures_and_algorithms_ii.shell.Shell()
 newShell.parser.parse_args()
 
-csv_file_path = data_structures_and_algorithms_ii.package_csv_file
-
 root = tkinter.Tk()
 app = data_structures_and_algorithms_ii.package_gui.CSVTableApp(
     root, data_structures_and_algorithms_ii.package_csv_file
