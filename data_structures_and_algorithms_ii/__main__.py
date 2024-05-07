19e3a95d74c842ff6104fb160f8dd88ba7210c21
Sheldon_Handler <57599804+Sheldon-Handler@users.noreply.github.com>
2024-05-07 14:51:04 -0400

    Removed unused code from __init__.py

--- data_structures_and_algorithms_ii/__main__.py
+++ data_structures_and_algorithms_ii/__main__.py
@@ -1,25 +1,8 @@
 import data_structures_and_algorithms_ii
 
 
-# newShell = data_structures_and_algorithms_ii.shell.Shell()
-# newShell.parser.parse_args()
-#
-# root = tkinter.Tk()
-# app = data_structures_and_algorithms_ii.package_gui.CSVTableApp(
-#     root, data_structures_and_algorithms_ii.package_csv_file
-# )
-# root.mainloop()
 def main():
 
     data_structures_and_algorithms_ii.read_csv_file.get_packages(
         data_structures_and_algorithms_ii.package_csv_file
     )
-
-    # newShell = data_structures_and_algorithms_ii.shell.Shell()
-    # newShell.parser.parse_args()
-    #
-    # root = tkinter.Tk()
-    # app = data_structures_and_algorithms_ii.package_gui.CSVTableApp(
-    #     root, data_structures_and_algorithms_ii.package_csv_file
-    # )
-    # root.mainloop()
