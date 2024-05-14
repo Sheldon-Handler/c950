89b9f5ff4661a49e0570b57a8621d234af74bd6b
Sheldon_Handler <57599804+Sheldon-Handler@users.noreply.github.com>
2024-05-14 12:53:45 -0400

    Removed unused deliver_together.py

--- data_structures_and_algorithms_ii/__main__.py
+++ data_structures_and_algorithms_ii/__main__.py
@@ -6,3 +6,5 @@ def main():
     data_structures_and_algorithms_ii.read_csv_file.get_packages(
         data_structures_and_algorithms_ii.package_csv_file
     )
+
+    print(data_structures_and_algorithms_ii.packages)
